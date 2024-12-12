77d375890d7de62060bdfd61fed4ea39c79a22bf
Soo <parksk971031@gmail.com>
2024-12-12 16:52:40 -0500

    ongoing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+// import { FaLocationArrow } from "react-icons/fa6";
+// import { Spotlight } from "./ui/Spotlight";
+// import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="pb-20 pt-36">
+        <Spotlight/>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
