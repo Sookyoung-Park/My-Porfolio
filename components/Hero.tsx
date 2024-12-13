8c97553600ac2e4f0bc939ee05e57f845f5d9e93
Soo <parksk971031@gmail.com>
2024-12-13 18:35:16 -0500

    fixed light&dark hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,38 +1,71 @@
+'use client'
+
 import { Spotlight } from "./ui/Spotlight";
 import { FlipWords } from "./ui/FlipWords";
+import { useTheme } from "next-themes";
+import { AuroraBackground } from "./ui/AuroraBackground";
+import { motion } from "framer-motion";
 // import MagicButton from "./ui/MagicButton";
 // import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
+  const { theme, setTheme } = useTheme();
+  const isDarkMode = theme === "dark";
+
   return (
-    <div className="pb-20 pt-36">
-    {/* spotlight */}
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
-      {/* Grid Background */}
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-        absolute top-0 left-0 flex items-center justify-center"
-      >
-        {/* Radial gradient for the container to give a faded look */}
-        <div
-        // chnage the bg to bg-black-100, so it matches the bg color and will blend in
-        className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-        bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-        />
-      </div>
-      <div className="flex justify-center">
+    <div>
+      {isDarkMode ?(
+        <div className="pb-40 pt-70">
+          <div>
+            {/* light blue */}
+            <Spotlight
+              className="-top-60 -left-10 md:-left-32 md:-top-20 h-screen"
+              fill="#3E8EF9"
+            />
+            {/* green */}
+            <Spotlight
+              className="h-[80vh] w-[80vw] top-25 left-full"
+              fill="#36D9B6"
+            />
+            {/* dark blue */}
+            <Spotlight className="left-80 top-28 h-[90vh] w-[50vw]" fill="#3D40F8" />
+          </div>
+          <div
+            className="h-screen w-full bg-black-100 bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center"
+          >
+            <div
+              className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100
+              [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+              />
+          </div>
+            <div className="flex justify-center">
+              <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 z-10">
+                    s00kyoung Park
+                </p>
+                <FlipWords
+                  words={["UI ENGINEER", "UI/UX DESIGNER"]}
+                  duration={1150}
+                  className="text-center text-[40px] md:text-5xl lg:text-6xl mt-10"
+                />
+            </div>
+          </div>
+        </div>
+      ):(
+        <AuroraBackground>
+          <motion.div
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.4,
+              duration: 4.5,
+              ease: "easeInOut",
+            }}
+            className="relative flex flex-col gap-4 items-center justify-center"
+          >
+            <div className="flex justify-center">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 z-10">
+            <p className="uppercase tracking-widest text-xs text-center text-blue-500 max-w-80 z-10">
                 s00kyoung Park
             </p>
             <FlipWords
@@ -40,23 +73,11 @@ const Hero = () => {
               duration={1150}
               className="text-center text-[40px] md:text-5xl lg:text-6xl mt-10"
             />
-            {/* Intro words */}
-            {/* <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl text-white z-10 mt-12">
-              Hi! I&apos;m Sookyoung Park, a UI Engineer/UIUX Designer based in USA and South Korea.
-            </p> */}
-            {/* Button
-            
-            -
-            +++++*/}
-            {/* <a href="#about">
-            <MagicButton
-              title="Show my work"
-              icon={<FaLocationArrow />}
-              position="right"
-            />
-          </a> */}
         </div>
       </div>
+      </motion.div>
+    </AuroraBackground>
+      )}
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -160,7 +160,15 @@ const config = {
   				to: {
   					transform: 'translate(calc(-50% - 0.5rem))'
   				}
-  			}
+  			},
+			aurora: {
+				from: {
+					backgroundPosition: "50% 50%, 50% 50%",
+				},
+				to: {
+					backgroundPosition: "350% 50%, 350% 50%",
+				},
+			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
@@ -172,7 +180,8 @@ const config = {
   			third: 'moveInCircle 40s linear infinite',
   			fourth: 'moveHorizontal 40s ease infinite',
   			fifth: 'moveInCircle 20s ease infinite',
-  			scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite'
+  			scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
+			aurora: "aurora 60s linear infinite",
   		}
   	}
   },
@@ -214,5 +223,7 @@ function addVariablesForColors({ addBase, theme }: any) {
     ":root": newVars,
   });
 }
+
+
 
 export default config;
